a8d95fe128412ceb3d6febdcac96f0ff42f4b36a
MieronHH <hailemelekot.mieron@gmail.com>
2023-07-18 18:20:06 -0400

    Frontend - react routing implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,17 @@
-import "./styles/_App.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-// importing components
-import Header from "./components/Header/header";
-import Search from "./components/Search/search";
-import Cards from "./components/Cards/cards";
-import Popular from "./components/Popular/popular";
-import Testimonials from "./components/Testimonials/testimonials";
-import Footer from "./components/Footer/footer";
+// importing pages
+import Home from "./pages/Home/Home";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+]);
 
 const App = () => {
-	return (
-		<div className="App">
-			<img
-				className="landing-page-image"
-				src={require("./images/landing_page_image.jpg")}
-				alt=""
-			/>
-			<div className="shade"></div>
-
-			<Header />
-			<Search />
-			<Cards />
-			<Popular />
-			<Testimonials />
-			<Footer />
-		</div>
-	);
+	return <RouterProvider router={router} />;
 };
 
 export default App;
